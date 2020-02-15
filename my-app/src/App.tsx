0e1b65f5ff7786b93599cd9e7aeb6219938f11e0
img31415 <karidy11@gmail.com>
2020-02-15 22:37:59 +0900

    setCount Counter function with reactHook / Typescript

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {TextField} from "./Text"
+import {Counter} from "./Counter";
 
 const App: React.FC = () => {
-  return <div>
-    <TextField text={"haha"} person={{firstName : "melissa", lastName:"goo"}}/>
-  </div>;
+    return <div>
+        {/* <TextField text={"haha"} person={{firstName : "melissa", lastName:"goo"}}/>*/}
+        <Counter>
+            {(count, setCount) => (<div>{count}
+                <button onClick={() => setCount(count + 1)}></button>
+            </div>)}
+        </Counter>
+    </div>;
 }
 
 export default App;
